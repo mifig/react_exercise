a39371df8abc206c19ff3aed01e000c981eb2a58
Miguel Figueiredo <miguelpfigueiredo@gmail.com>
2022-05-06 12:14:16 +0100

    first react app done

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.css";
-import Counter from "./counter";
+import Counter from "./Counter";
+import { useState } from 'react';
 
 function App() {
+  const [counters, setCounters] = useState([
+    { id: 1, initial: 0 },
+    { id: 2, initial: 10, step: 2 },
+    { id: 3, initial: 3, step: 5 }
+  ]);
 
+  const handleAddCounter = () => {
+    setCounters([{ id: new Date().getTime(), initial: 0 }, ...counters]);
+  };
+
+  const handleRemoveCounter = (id) => {
+    setCounters(counters.filter((counter) => counter.id !== id));
+  };
 
   return ( 
     <div className='my-2 text-center'>
       <h1>My counters:</h1>
-      <Counter />
-      <Counter />
+      <button className="btn btn-primary" onClick={handleAddCounter}>+ Add counter</button>
+      <hr></hr>
+      {counters.map(({ id, ...props }) => {
+        return (
+          <div key={id}>
+            <Counter id={id} {...props} onDelete={handleRemoveCounter} />
+          </div>
+        );
+      })}
     </div>
   );
-}
+};
 
 export default App;
--- src/counter.jsx
+++ src/counter.jsx
@@ -1,24 +1,39 @@
 import { useState } from 'react';
 
-function Counter() {
-  let [counter, setCounter] = useState(0);
+function Counter({ id, initial, step = 1, onDelete }) {
+  let [counter, setCounter] = useState(initial);
+  let [disabled, setDisabled] = useState(false);
 
-  const handleSumCounter = () => {
-    setCounter(counter += 1);
+  const handleSumCounter = (event) => {
+    setCounter(counter + step);
+    setDisabled(disabled = false);
+    setDisabled(event.currentTarget.parentNode.children[0].disabled = disabled);
   };
 
-  const handleSubCounter = () => {
+  const handleSubCounter = (event) => {
     if(counter > 0) {
-      setCounter(counter -= 1);
-    }
+      setCounter(Math.max(counter - step,0));
+    } else {
+      setDisabled(disabled = true)
+      setDisabled(event.currentTarget.disabled = disabled);
+    };
+  };
+
+  const handleDelete = () => {
+    onDelete(id);
   };
 
   return (
-    <div className="mt-5 d-flex justify-content-center">
-      <button className="btn btn-success me-1" onClick={handleSubCounter}>-</button>
-      <p className="m-0">{counter}</p>
-      <button className="btn btn-danger ms-1" onClick={handleSumCounter}>+</button>
-    </div>
+    <>
+      <div className="mt-5 d-flex justify-content-center">
+        <button className="btn btn-danger me-1" onClick={handleSubCounter}>-</button>
+        <div className='mx-2 d-flex align-items-center'>
+          <p className="m-0">{counter}</p>
+        </div>
+        <button className="btn btn-success ms-1" onClick={handleSumCounter}>+</button>
+      </div>
+      <button className="btn btn-warning mt-2" onClick={handleDelete}>remove counter</button>
+    </>
   );
 };
 
